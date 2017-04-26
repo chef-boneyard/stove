440302db1e2c882d28a3cc7039cb522ec6839ad0
Noah Kantrowitz <noah@coderanger.net>
2017-04-25 20:38:10 -0700

    Some refactoring in case we want to support yank on artifactory in the future.

--- lib/stove/artifactory.rb
+++ lib/stove/artifactory.rb
@@ -11,45 +11,72 @@ module Stove
     #   the cookbook to upload
     #
     def upload(cookbook, extended_metadata = false)
-      base_uri_string = Config.artifactory.strip
-      base_uri_string << '/' unless base_uri_string.end_with?('/')
-      base_uri = URI(base_uri_string)
-
-      # Open the HTTP connection to artifactory.
-      connection = Net::HTTP.new(base_uri.host, base_uri.port)
-      if base_uri.scheme == 'https'
-        connection.use_ssl = true
-        # Mimic the behavior of the Cookbook uploader for SSL verification.
-        if ENV['STOVE_NO_SSL_VERIFY'] || !Config.ssl_verify
-          connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        end
+      # Artifactory doesn't prevent uploading over an existing release in
+      # some cases so let's check for that. Seriously never do this, go delete
+      # and then re-upload if you have to.
+      response = request(:get, "api/v1/cookbooks/#{cookbook.name}/versions/#{cookbook.version}")
+      # Artifactory's version of the cookbook_version endpoint returns an
+      # empty 200 on an unknown version.
+      unless response.code == '404' || (response.code == '200' && response.body.to_s == '')
+        raise Error::CookbookAlreadyExists.new(cookbook: cookbook)
       end
 
-      connection.start do |http|
-        # Artifactory doesn't prevent uploading over an existing release in
-        # some cases so let's check for that. Seriously never do this, go delete
-        # and then re-upload if you have to.
-        check_uri = URI("#{base_uri}api/v1/cookbooks/#{cookbook.name}/versions/#{cookbook.version}")
-        check_request = Net::HTTP::Get.new(check_uri)
-        check_request['X-Jfrog-Art-Api'] = Config.artifactory_key.strip
-        response = http.request(check_request)
-        # Artifactory's version of the cookbook_version endpoint returns an
-        # empty 200 on an unknown version.
-        if response.code != '200' || !response.body.empty?
-          raise Error::CookbookAlreadyExists.new(cookbook: cookbook)
-        end
-
-        upload_uri = URI("#{base_uri}api/v1/cookbooks/#{cookbook.name}.tgz")
-        upload_request = Net::HTTP::Post.new(upload_uri)
-        upload_request.body_stream = cookbook.tarball(extended_metadata)
-        upload_request.content_length = upload_request.body_stream.size
-        upload_request['Content-Type'] = 'application/x-binary'
-        upload_request['X-Jfrog-Art-Api'] = Config.artifactory_key.strip
-        response = http.request(upload_request)
-        if response.code != '201'
-          response.error!
-        end
+      # Run the upload.
+      response = request(:post, "api/v1/cookbooks/#{cookbook.name}.tgz") do |req|
+        req.body_stream = cookbook.tarball(extended_metadata)
+        req.content_length = req.body_stream.size
+        req['Content-Type'] = 'application/x-binary'
       end
+      response.error! unless response.code == '201'
+    end
+
+    private
+
+    #
+    # Create an HTTP connect to the Artifactory server.
+    #
+    # @return [Net::HTTP]
+    #
+    def connection
+      @connection ||= begin
+        uri = URI(Config.artifactory.strip)
+        # Open the HTTP connection to artifactory.
+        http = Net::HTTP.new(uri.host, uri.port)
+        if uri.scheme == 'https'
+          http.use_ssl = true
+          # Mimic the behavior of the Cookbook uploader for SSL verification.
+          if ENV['STOVE_NO_SSL_VERIFY'] || !Config.ssl_verify
+            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          end
+        end
+        http.start
+      end
+    end
+
+    #
+    # Send an HTTP request to the Artifactory server.
+    #
+    # @param [Symbol] method
+    #   HTTP method to use
+    #
+    # @param [String] path
+    #   URI path to request
+    #
+    # @param [Proc] block
+    #   Optional block to set request values
+    #
+    # @return [Net::HTTPResponse]
+    #
+    def request(method, path, &block)
+      uri_string = Config.artifactory.strip
+      # Make sure we end up with the right number of separators.
+      uri_string << '/' unless uri_string.end_with?('/')
+      uri_string << path
+      uri = URI(uri_string)
+      request = Net::HTTP.const_get(method.to_s.capitalize).new(uri)
+      request['X-Jfrog-Art-Api'] = Config.artifactory_key.strip
+      block.call(request) if block
+      connection.request(request)
     end
 
   end
